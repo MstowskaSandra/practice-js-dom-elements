89c02b2f94f5386e043dc2d2d561293a1e7d651e
Sandra <sandra.mstowskaa@gmail.com>
2025-08-21 17:42:14 +0200

    complete task 03

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,20 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+
+const btn = document.createElement('button');
+btn.innerText = buttonSettings.text;
+
+for(let key in buttonSettings.attr) {
+    btn[key] = buttonSettings.attr[key]
+}
+
+for(let key in buttonSettings.css) {
+    btn.style[key] = buttonSettings.css[key];
+}
+
+console.log(btn);
+
+const buttonParent = document.querySelector('.parent-for-button');
+buttonParent.appendChild(btn);
