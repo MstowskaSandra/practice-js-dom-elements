7f32e51497e634b18589985e17fc179ba1968dbe
Sandra <sandra.mstowskaa@gmail.com>
2025-08-21 15:37:21 +0200

    complete task 02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,9 @@
 console.log('DOM');
+
+const linkList = document.querySelectorAll('[data-url]');
+
+linkList.forEach(link => {
+    const url = link.getAttribute('data-url');
+    link.href = url;
+    console.log("Gotowe url:", link.href);
+});
