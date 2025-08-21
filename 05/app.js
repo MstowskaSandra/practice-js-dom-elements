f4536d9b2a6770d6dbf37c97546c902f4e19162c
Sandra <sandra.mstowskaa@gmail.com>
2025-08-21 19:09:21 +0200

    complete task 05/ complete all tasks

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,30 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+const newBtn = document.createElement('button');
+newBtn.innerText = 'Usuń z koszyka';
+curr.after(newBtn);
+
+const parent = curr.parentElement;
+
+for(let child of parent.children) {
+    if( child !== curr) {
+        child.classList.add('siblings');
+    }
+}
+
+const nextArticle = parent.nextElementSibling;
+nextArticle.setAttribute('title', 'nextElementSibling');
+
+const lastArticle = nextArticle.nextElementSibling;
+const nextP = document.createElement('p');
+lastArticle.insertBefore(nextP, lastArticle.lastElementChild);
+
+
+const cloneArticle = parent.cloneNode(true);
+const section = parent.parentElement;
+section.insertBefore(cloneArticle, section.firstElementChild);
+
+console.log(section);
+
