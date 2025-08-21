ae6541146e40fc6609d89f986c55ad083cff9659
Sandra <sandra.mstowskaa@gmail.com>
2025-08-21 15:18:36 +0200

    complete task 01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const elementList = document.querySelector('.comments__item.comments__item--newest');
+
+const elementWithAttribute = elementList.querySelectorAll('[data-info]');
+
+console.log("Liczba pasujących elementów:", elementWithAttribute.length);
+
